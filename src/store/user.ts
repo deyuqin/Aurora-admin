365e334ea7a3209084c5a49818e1744ee1ff168a
qindeyu <401623837@qq.com>
2022-07-18 16:07:57 +0800

    🎉 init: init

---
+++ src/store/user.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore({
+  id: 'user', //id必填，且需要唯一
+  state: () => {
+    return {
+      name: 'zh',
+    };
+  },
+
+  actions: {},
+});
